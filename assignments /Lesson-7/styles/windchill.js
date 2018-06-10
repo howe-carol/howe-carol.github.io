8ffb4ecccede1f70be947af31ef8065aa5dfbc44
Carol Howe <jchowe2015@gmail.com>
2018-06-09 22:54:41 -0600

    f

---
+++ assignments /Lesson-7/styles/windchill.js
@@ -0,0 +1,12 @@
+
+
+{
+var t = 70;
+var s = 7;
+var y = Math.pow(s, 0.16);
+var f = 35.74 + 0.6215 * t - 35.75 * y + 0.4275 * t * y;
+var x = f.toFixed(2);
+document.getElementById('windchill').innerHTML = 
+"Wind Chill Factor: " + x + "&deg;F";
+}
+
